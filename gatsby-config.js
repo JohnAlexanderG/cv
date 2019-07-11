d8fbdc1c706249ab3d9f77ad562742adad44b849
Jhon Alexander <gddesignw@gmail.com>
2019-07-11 10:59:20 -0500

    modificando color del encabezado en el manifest.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,8 +21,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#c5c5c5`,
-        theme_color: `#c5c5c5`,
+        background_color: `#1e1e1e`,
+        theme_color: `#1e1e1e`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
